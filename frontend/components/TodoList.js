08956f3147fcab52b94819e79b5b06d3801579d4
Christian Anderson <tehninjawaffle@gmail.com>
2023-08-31 14:34:27 -0700

    adde functionality to hide completed 'todo's'

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -4,16 +4,26 @@ import Todo from "./Todo"
 export default class TodoList extends React.Component {
 
   // Receive the Todo.js array, then map over it, generating a new <Todo /> for each element
+  state = {
+    showAll: true
+  }
 
-
+  toggleShowAll = evt => {
+    this.setState({
+      ...this.state,
+      showAll: !this.state.showAll,
+    })
+  }
   render() {
     const { todos, handleCompleted } = this.props
+    const filtered = todos.filter(val => val.completed || this.state.showAll)
     return (
       <div>
-        {todos.map((val) => (
+        {filtered.map((val) => (
           <Todo id={val.id} name={val.name} completed={!val.completed} handleCompleted={handleCompleted} />
         ))
         }
+        <button onClick={this.toggleShowAll}>Hide Completed</button>
         
       </div>
     )
